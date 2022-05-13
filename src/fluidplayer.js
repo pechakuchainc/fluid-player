a8444584f37a10ec21815eb9ee114139cc577c7c
John Brooks <johnbuckbrooks@gmail.com>
2022-05-13 21:20:18 +0000

    ...

--- src/fluidplayer.js
+++ src/fluidplayer.js
@@ -385,8 +385,8 @@ const fluidPlayerClass = function () {
         // Streamers will re-initialize once ad has been shown.
         const preRolls = self.findRoll("preRoll");
         if (!preRolls || 0 === preRolls.length) {
-            console.log("FLUID init initialiseStreamers");
-            self.initialiseStreamers();
+            // console.log("FLUID init initialiseStreamers");
+            // self.initialiseStreamers();
         }
 
         const _play_videoPlayer = playerNode.play;
